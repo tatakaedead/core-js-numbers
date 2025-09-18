429d960534569ce17c352aa1c6e5100c66fd38d2
Ivan <vaniamar@mail.ru>
2025-09-19 05:12:52 +0700

    add new solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -406,8 +406,8 @@ function toPrecision(number, precision) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
@@ -472,8 +472,8 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
